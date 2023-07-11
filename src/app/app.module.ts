0b11cb40c57be96dadbac350ca7a3738a9dd4cab
lyeslimani <lslimani@myges.fr>
2023-07-11 21:18:48 +0200

    chore: setup tailwing, ngmat and ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule],
+	imports: [
+		BrowserModule,
+		StoreModule.forRoot({}, {}),
+		BrowserAnimationsModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
---
+++ src/app/shared/interfaces/pokemon.ts
@@ -0,0 +1,37 @@
+export interface Pokemon {
+	number: number;
+	name: string;
+	types: number;
+	type1: string;
+	type2: string;
+	height: number;
+	weight: number;
+	malePct: number;
+	femalePct: number;
+	captRate: number;
+	expPoints: number;
+	expSpeed: string;
+	baseTotal: number;
+	hp: number;
+	attack: number;
+	defense: number;
+	special: number;
+	speed: number;
+	normalDmg: number;
+	fireDmg: number;
+	waterDmg: number;
+	electricDmg: number;
+	grassDmg: number;
+	iceDmg: number;
+	fightDmg: number;
+	poisonDmg: number;
+	groundDmg: number;
+	flyingDmg: number;
+	psychicDmg: number;
+	bugDmg: number;
+	rockDmg: number;
+	ghostDmg: number;
+	dragonDmg: number;
+	evolutions: number;
+	legendary: number;
+}
