98f506ebf361edc0eaa0a866cb6275bd626f65b9
Fanny Guillemont <fanny.gt10@gmail.com>
2023-07-15 15:27:58 +0200

    fix: comments

--- src/app/shared/tools/getPokemonPhotoUrl.ts
+++ src/app/shared/tools/getPokemonPhotoUrl.ts
@@ -2,11 +2,9 @@ import { Pokemon } from '../interfaces/pokemon';
 
 export const getImageUrl = (pokemon: Pokemon) => {
 	if (pokemon.image == null) {
-		console.log(`ici null`);
 		const id = pokemon.number.toString().padStart(3, `0`);
 		return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
 	} else {
-		console.log(`ici pas null`);
 		return pokemon.image;
 	}
 };
